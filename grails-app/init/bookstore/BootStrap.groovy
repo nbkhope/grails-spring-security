8e298911e2f59f3d0b587dd67fee0110299a04c2
Renan Martins <martins@fujisan.co.jp>
2017-01-05 15:06:23 -0800

    Add initial roles and a test user

--- grails-app/init/bookstore/BootStrap.groovy
+++ grails-app/init/bookstore/BootStrap.groovy
@@ -3,6 +3,27 @@ package bookstore
 class BootStrap {
 
     def init = { servletContext ->
+
+    	// Create roles: admin and user
+    	def adminRole = new Role(authority: 'ROLE_ADMIN').save(failOnError: true)
+    	def userRole = new Role(authority: 'ROLE_USER').save(failOnError: true)
+
+    	def testUser = new User(username: 'ana', password: '123456').save(failOnError: true)
+
+    	// Create relationship between user and role
+    	UserRole.create testUser, adminRole
+
+    	// We explicitly flush (using withSession) because BootStrap 
+    	// does not run in a transaction or OpenSessionInView.
+    	UserRole.withSession {
+    		it.flush()
+    		it.clear()
+    	}
+
+    	assert User.count() == 1
+    	assert Role.count() == 2
+    	assert UserRole.count() == 1
+
     }
     def destroy = {
     }
